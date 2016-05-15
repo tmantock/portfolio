2b97df3285859c2672b91bb593854114636e7151
Tevin Mantock <tmantock@gmail.com>
2016-05-14 19:58:25 -0700

    added scroll function and project list

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
 $(document).ready(function (){
-    var x = $('#projectTwo').scrollTop();
+    
 });
+
+function pageScroll (element) {
+    var navPosition = $('#navbar').position().top;
+    var project = $(element).attr('data-project');
+    var id = '#' + project;
+    var projectPosition = $(id).position().top;
+    
+    $(id).animate({top:(navPosition + projectPosition) * -1 + 'px'},1000);
+}
