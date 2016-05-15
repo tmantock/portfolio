5da907c6a837fa2afe67472f9208fb337d54a90a
Tevin Mantock <tmantock@gmail.com>
2016-05-15 00:18:11 -0700

    changed the scroll function to allow smooth continuous scroll

--- script.js
+++ script.js
@@ -3,10 +3,9 @@ $(document).ready(function (){
 });
 
 function pageScroll (element) {
-    var navPosition = $('#navbar').position().top;
     var project = $(element).attr('data-project');
     var id = '#' + project;
     var projectPosition = $(id).position().top;
     
-    $(id).animate({top:(navPosition + projectPosition) * -1 + 'px'},1000);
+    $('.projectContainer').animate({top:(projectPosition) * -1 + 'px'},1000);
 }
