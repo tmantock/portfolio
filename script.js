4e08432d13c1297f968203908cc88f71a1cdaf21
Tevin Mantock <tmantock@gmail.com>
2016-05-15 01:00:42 -0700

    changed functions and css for the list to indicate the chosen project

--- script.js
+++ script.js
@@ -4,8 +4,15 @@ $(document).ready(function (){
 
 function pageScroll (element) {
     var project = $(element).attr('data-project');
+    var listItem = $('.projectTitle');
     var id = '#' + project;
     var projectPosition = $(id).position().top;
+
+    if(listItem.hasClass('selectProject')) {
+        listItem.removeClass('selectProject');
+    }
     
-    $('.projectContainer').animate({top:(projectPosition) * -1 + 'px'},1000);
+    $(element).addClass('selectProject');
+    
+    $('.projectContainer').animate({top:(projectPosition) * -1 + 'px'},1300);
 }
