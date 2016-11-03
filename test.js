ca97ed7e01b0a542da8db3e43a120d7907de9e06
Tevin Mantock <tmantock@gmail.com>
2016-11-03 08:42:52 -0700

    Updated test.js

--- test.js
+++ test.js
@@ -3,12 +3,12 @@ $("#box").css({
   top: height + 10,
   position: 'absolute'
 });
-function timeAdd(start,stop){
+function timeAdd(element, start,stop){
   if(typeof(start)!=='number' && typeof(stop)!=='number' || start === stop){
     return; 
   }
   start++;
-  $("#box").text(start);
+  element.text(start);
   setTimeout(function(){timeAdd(start,stop);}, 65);
 }
 
